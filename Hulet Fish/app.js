460ebc1e4c5314e1c6c9b30a41fbce68605c297b
Habtamu Bogale <abubogale342@gmail.com>
2025-11-06 10:02:56 +0300

    fix: add_cors_origin

--- Hulet Fish/app.js
+++ Hulet Fish/app.js
@@ -26,7 +26,8 @@ const allowedOrigins = process.env.FRONTEND_URL
       'http://localhost:8080',
       'http://localhost:5173',
       'http://localhost:3000',
-      'http://127.0.0.1:8080'
+      'http://127.0.0.1:8080',
+      'https://hulet-fish-1-ofen.vercel.app'
     ];
 
 app.use(
